49b8e5a5e6ba91f2a9d5e6a5c04fe981bd36206d
Artis Brizs <artis.brizs@student.griffith.ie>
2021-03-07 11:59:13 +0000

    adding files

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
@@ -7,6 +7,54 @@ public class File {
     private String filename;
     private String contenttype;
     private String filesize;
-    private User user;
+    private Integer userid;
     private Blob filedata;
+
+    public Integer getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Integer fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getContenttype() {
+        return contenttype;
+    }
+
+    public void setContenttype(String contenttype) {
+        this.contenttype = contenttype;
+    }
+
+    public String getFilesize() {
+        return filesize;
+    }
+
+    public void setFilesize(String filesize) {
+        this.filesize = filesize;
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public Blob getFiledata() {
+        return filedata;
+    }
+
+    public void setFiledata(Blob filedata) {
+        this.filedata = filedata;
+    }
 }
